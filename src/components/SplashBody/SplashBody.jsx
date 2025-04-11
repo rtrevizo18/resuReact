c3a3a8b1af8fb095543b4f359c82eea788e77c3d
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-04-11 02:11:11 -0500

    feat(splashbody): added basic splash page elements and styling

--- src/components/SplashBody/SplashBody.jsx
+++ src/components/SplashBody/SplashBody.jsx
@@ -4,7 +4,15 @@ export default function SplashBody(){
     return (
         <div id="splash-body">
             <div id="splash-welcome">
-                <h1>Title</h1>
+                <h2 class="splash-welcome-title">
+                    Resume Troubles?<br/>
+                    Not getting calls back?<br/>
+                    Low ATS scores?<br/>
+                    </h2>
+                <div class="splash-welcome-desc">
+                    Introducing ResuReact, the all-new web application<br />
+                    that provides a fully realized, formatted resume.
+                </div>
             </div>
         </div>
     )
