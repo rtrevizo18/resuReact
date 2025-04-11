64309ba112cb63bef5117cf543d8ccae9841f9cc
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-04-11 01:15:17 -0500

    feat: add SplashBody component for splash page
    Also added some minor styling and reshuffled the Body component to be general

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,9 +1,10 @@
 import ResumeCards from "../ResumeCards/ResumeCards";
+import SplashBody from "../SplashBody/SplashBody";
 import "./Body.css";
 export default function Body() {
   return (
     <div id="body">
-      <ResumeCards />
+      <SplashBody />
     </div>
   );
 }
---
+++ src/components/SplashBody/SplashBody.jsx
@@ -0,0 +1,11 @@
+import "./SplashBody.css";
+
+export default function SplashBody(){
+    return (
+        <div id="splash-body">
+            <div id="splash-welcome">
+                <h1>Title</h1>
+            </div>
+        </div>
+    )
+}
