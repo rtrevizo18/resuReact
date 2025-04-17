4ee6057e5838da0122191ff2a5907b88f7a70285
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-04-16 23:41:02 -0500

    feat: add useMultiStepForm to organize state logic with multi-step forms

---
+++ src/useMultiStepForm.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export function useMultiStepForm(steps) {
+  const [currentStepIndex, setCurrentStepIndex] = useState(0);
+
+  function next() {
+    setCurrentStepIndex((i) => {
+      if (i >= steps.length - 1) return i;
+      return i + 1;
+    });
+  }
+
+  function back() {
+    setCurrentStepIndex((i) => {
+      if (i <= 0) return i;
+      return i - 1;
+    });
+  }
+
+  function goTo(index) {
+    setCurrentStepIndex(index);
+  }
+
+  return {
+    currentStepIndex,
+    step: steps[currentStepIndex],
+    steps,
+    goTo,
+    next,
+    back,
+  };
+}
